f5405abfbedd92ca5ea067665e5bf9d0a9b1be02
rncbc <rncbc@449dfdf3-0c07-481a-a240-0981d7471adc>
2012-10-24 16:56:43 +0000

    - Slight minor fix on fast-release re-triggering.
    git-svn-id: http://svn.code.sf.net/p/synthv1/code_attic/trunk@24 449dfdf3-0c07-481a-a240-0981d7471adc

--- src/synthv1.cpp
+++ src/synthv1.cpp
@@ -177,10 +177,9 @@ struct synthv1_env
 	{
 		p->running = true;
 		p->stage = Release;
-		if (p->frames > min_frames) {
+		if (p->frames > min_frames)
 			p->frames = min_frames;
-			p->delta = -(p->level) / float(p->frames);
-		}
+		p->delta = -(p->level) / float(p->frames);
 	}
 
 	// parameters
