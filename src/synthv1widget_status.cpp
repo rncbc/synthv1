b41a9e900637e1fc8bbf95a73595fded4edcb598
rncbc <rncbc@449dfdf3-0c07-481a-a240-0981d7471adc>
2012-12-08 12:18:59 +0000

    - Make custom status-bar widget.
    git-svn-id: http://svn.code.sf.net/p/synthv1/code_attic/trunk@68 449dfdf3-0c07-481a-a240-0981d7471adc

---
+++ src/synthv1widget_status.cpp
@@ -0,0 +1,40 @@
+// synthv1widget_status.cpp
+//
+/****************************************************************************
+   Copyright (C) 2012, rncbc aka Rui Nuno Capela. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*****************************************************************************/
+
+#include "synthv1widget_status.h"
+
+#include "synthv1widget_config.h"
+
+#include <QLabel>
+
+
+//-------------------------------------------------------------------------
+// synthv1widget_status - Custom status-bar widget.
+//
+
+// Constructor.
+synthv1widget_status::synthv1widget_status ( QWidget *pParent )
+	: QStatusBar (pParent)
+{
+}
+
+
+// end of synthv1widget_status.cpp
---
+++ src/synthv1widget_status.h
@@ -0,0 +1,47 @@
+// synthv1widget_status.h
+//
+/****************************************************************************
+   Copyright (C) 2012, rncbc aka Rui Nuno Capela. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*****************************************************************************/
+
+#ifndef __synthv1widget_status_h
+#define __synthv1widget_status_h
+
+#include <QStatusBar>
+
+// Forward declarations.
+class QLabel;
+
+
+//-------------------------------------------------------------------------
+// synthv1widget_status - Custom status-bar widget.
+
+class synthv1widget_status : public QStatusBar
+{
+	Q_OBJECT
+
+public:
+
+	// Constructor.
+	synthv1widget_status(QWidget *pParent = 0);
+};
+
+
+#endif  // __synthv1widget_status_h
+
+// end of synthv1widget_status.h
