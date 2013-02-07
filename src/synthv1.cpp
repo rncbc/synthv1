1c89c4fd2d8feb87a328435ec02cb05948972cb8
rncbc <rncbc@449dfdf3-0c07-481a-a240-0981d7471adc>
2013-02-07 15:52:32 +0000

    - Improved filters parameter sensitivity (cutoff, reso). (yaFIX)
    git-svn-id: http://svn.code.sf.net/p/synthv1/code_attic/trunk@99 449dfdf3-0c07-481a-a240-0981d7471adc

--- src/synthv1.cpp
+++ src/synthv1.cpp
@@ -91,7 +91,7 @@ inline float synthv1_sigmoid_0 ( const float x, const float t0 )
 #endif
 }
 
-inline float synthv1_sigmoid_1 ( const float x, const float t0 = 0.001f )
+inline float synthv1_sigmoid_1 ( const float x, const float t0 = 0.01f )
 {
 	return 0.5f * (1.0f + synthv1_sigmoid_0(2.0f * x - 1.0f, t0));
 }
