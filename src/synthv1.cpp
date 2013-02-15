b69135dc2d36d3a5afe1548beedd043c2d14c929
rncbc <rncbc@449dfdf3-0c07-481a-a240-0981d7471adc>
2013-02-15 18:41:13 +0000

    - Sustain/damper pedal switch (MIDI CC#64) support added. (FIX)
    git-svn-id: http://svn.code.sf.net/p/synthv1/code_attic/trunk@105 449dfdf3-0c07-481a-a240-0981d7471adc

--- src/synthv1.cpp
+++ src/synthv1.cpp
@@ -1213,7 +1213,7 @@ void synthv1_impl::process_midi ( uint8_t *data, uint32_t size )
 	// note on
 	if (status == 0x90 && value > 0) {
 		synthv1_voice *pv = m_notes[key];
-		if (pv && !m_ctl.sustain) {
+		if (pv/* && !m_ctl.sustain*/) {
 			// retrigger fast release
 			m_dcf1.env.note_off_fast(&pv->dcf1_env);
 			m_dcf2.env.note_off_fast(&pv->dcf2_env);
