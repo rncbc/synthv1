595416924db53f604fb4f711a7fb7957b1eb8a5d
rncbc <rncbc@449dfdf3-0c07-481a-a240-0981d7471adc>
2013-04-10 20:23:54 +0000

    - A slight GUI behavior change: (re)writing existing names   doesn't load the respective preset anymore. (FIX)
    git-svn-id: http://svn.code.sf.net/p/synthv1/code_attic/trunk@121 449dfdf3-0c07-481a-a240-0981d7471adc

--- src/synthv1widget_preset.cpp
+++ src/synthv1widget_preset.cpp
@@ -192,7 +192,7 @@ bool synthv1widget_preset::queryPreset (void)
 // Preset management slots...
 void synthv1widget_preset::activatePreset ( const QString& sPreset )
 {
-	if (!sPreset.isEmpty() && !queryPreset())
+	if (!sPreset.isEmpty() && queryPreset())
 		loadPreset(sPreset);
 }
 
