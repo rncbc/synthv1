a8d6862ce3e72a84a4a40b64d8563306459e94e7
rncbc <rncbc@449dfdf3-0c07-481a-a240-0981d7471adc>
2013-05-29 19:07:17 +0000

    - LV2 External UI widget gets raised and activated when shown.
    git-svn-id: http://svn.code.sf.net/p/synthv1/code_attic/trunk@144 449dfdf3-0c07-481a-a240-0981d7471adc

--- src/synthv1widget_lv2.cpp
+++ src/synthv1widget_lv2.cpp
@@ -166,8 +166,14 @@ static void synthv1_lv2ui_external_show ( LV2_External_UI_Widget *ui_external )
 {
 	synthv1_lv2ui_external_widget *pExtWidget
 		= (synthv1_lv2ui_external_widget *) (ui_external);
-	if (pExtWidget && pExtWidget->widget)
-		pExtWidget->widget->show();
+	if (pExtWidget) {
+		synthv1widget_lv2 *widget = pExtWidget->widget;
+		if (widget) {
+			widget->show();
+			widget->raise();
+			widget->activateWindow();
+		}
+	}
 }
 
 static void synthv1_lv2ui_external_hide ( LV2_External_UI_Widget *ui_external )
