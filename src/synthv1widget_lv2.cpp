270ed2b2669fad5597cf3e853d805f847f9cf9a5
rncbc <rncbc@449dfdf3-0c07-481a-a240-0981d7471adc>
2014-06-28 19:33:51 +0000

    - LV2 UI Idle and Show interfaces support added. (EXPERIMENTAL)
    git-svn-id: http://svn.code.sf.net/p/synthv1/code_attic/trunk@269 449dfdf3-0c07-481a-a240-0981d7471adc

--- src/synthv1widget_lv2.cpp
+++ src/synthv1widget_lv2.cpp
@@ -193,13 +193,14 @@ static void synthv1_lv2ui_port_event (
 		pWidget->port_event(port_index, buffer_size, format, buffer);
 }
 
+
 #ifdef CONFIG_LV2_UI_IDLE
 
 int synthv1_lv2ui_idle ( LV2UI_Handle ui )
 {
 	synthv1widget_lv2 *pWidget = static_cast<synthv1widget_lv2 *> (ui);
 	if  (pWidget && !pWidget->isIdleClosed()) {
-		QApplication::processEvents();
+	//	QApplication::processEvents();
 		return 0;
 	} else {
 		return 1;
