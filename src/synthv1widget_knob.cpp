1f6ba1b0c9defb6aaa2c80769cce7e424deb7186
rncbc <rncbc@449dfdf3-0c07-481a-a240-0981d7471adc>
2014-11-03 21:08:05 +0000

    - Fixed parameters A/B comparison swap. (EXPERIMENTAL)
    git-svn-id: http://svn.code.sf.net/p/synthv1/code_attic/trunk@291 449dfdf3-0c07-481a-a240-0981d7471adc

--- src/synthv1widget_knob.cpp
+++ src/synthv1widget_knob.cpp
@@ -297,13 +297,13 @@ void synthv1widget_spin::setSingleStep ( float fSingleStep )
 
 QString synthv1widget_spin::valueText (void) const
 {
-	return QString::number(m_pSpinBox->value());
+	return QString::number(value());
 }
 
 
 float synthv1widget_spin::value (void) const
 {
-	return float(m_pSpinBox->value());
+	return valueFromScale(m_pSpinBox->value());
 }
 
 
