490fc02c73cf1ff78ec7b23ae446e1da376e70d8
rncbc <rncbc@gamma.rncbc.lan>
2016-05-06 22:42:31 +0100

    - Ditched most of legacy cruft. (2)

--- src/synthv1_ui.cpp
+++ src/synthv1_ui.cpp
@@ -39,7 +39,7 @@ synthv1 *synthv1_ui::instance (void) const
 
 void synthv1_ui::setParamValue ( synthv1::ParamIndex index, float fValue )
 {
-	m_pSynth->setParamValue(index, fValue, true); // Cached!
+	m_pSynth->setParamValue(index, fValue); // Cached!
 }
 
 float synthv1_ui::paramValue ( synthv1::ParamIndex index ) const
