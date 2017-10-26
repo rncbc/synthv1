f048218059cea894f314b6f57d15f4082f5c82b8
rncbc <rncbc@rncbc.org>
2017-10-26 17:42:07 +0100

    - Last minute fixing on native file-open/save dialogs...

--- src/synthv1widget_preset.cpp
+++ src/synthv1widget_preset.cpp
@@ -236,11 +236,7 @@ void synthv1widget_preset::openPreset (void)
 	const QString& sTitle  = tr("Open Preset") + " - " SYNTHV1_TITLE;
 	const QString& sFilter = tr("Preset files (*.%1)").arg(sExt);
 
-#if QT_VERSION < 0x040400
 	QWidget *pParentWidget = NULL;
-#else
-	QWidget *pParentWidget = nativeParentWidget();
-#endif
 	QFileDialog::Options options = 0;
 	if (pConfig->bDontUseNativeDialogs) {
 		options |= QFileDialog::DontUseNativeDialog;
@@ -308,11 +304,7 @@ void synthv1widget_preset::savePreset ( const QString& sPreset )
 	if (!fi.exists()) {
 		const QString& sTitle  = tr("Save Preset") + " - " SYNTHV1_TITLE;
 		const QString& sFilter = tr("Preset files (*.%1)").arg(sExt);
-	#if QT_VERSION < 0x040400
 		QWidget *pParentWidget = NULL;
-	#else
-		QWidget *pParentWidget = nativeParentWidget();
-	#endif
 		QFileDialog::Options options = 0;
 		if (pConfig->bDontUseNativeDialogs) {
 			options |= QFileDialog::DontUseNativeDialog;
