98d887fa8d253b9ca06405c391fc7b3770b86e21
rncbc <rncbc@rncbc.org>
2018-02-14 15:47:40 +0000

    - Make NSM session always dirty on anything but loading/saving a NSM session.

--- src/synthv1widget_jack.cpp
+++ src/synthv1widget_jack.cpp
@@ -130,7 +130,7 @@ void synthv1widget_jack::updateDirtyPreset ( bool bDirtyPreset )
 
 #ifdef CONFIG_NSM
 	if (m_pNsmClient && m_pNsmClient->is_active())
-		m_pNsmClient->dirty(bDirtyPreset);
+		m_pNsmClient->dirty(true); // as far as NSM goes, we're always filthy!
 #endif
 }
 
