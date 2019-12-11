082505ba2f480e2b61b38160d0f3e00c1d8ea24c
rncbc <rncbc@rncbc.org>
2019-12-11 15:21:21 +0000

    - Fixing on translations compliance to hard-coded strings.

--- src/synthv1widget_config.cpp
+++ src/synthv1widget_config.cpp
@@ -44,13 +44,8 @@
 #include <QStyleFactory>
 
 
-// Translatable macro contextualizer.
-#undef  _TR
-#define _TR(x) QT_TR_NOOP(x)
-
-
 // Default (empty/blank) name.
-static const char *g_pszDefName = _TR("(default)");
+static const char *g_pszDefName = QT_TRANSLATE_NOOP("synthv1widget_config", "(default)");
 
 
 //----------------------------------------------------------------------------
