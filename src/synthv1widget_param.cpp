fe44f3c04cc5990ec56064980691fd5752573ba6
rncbc <rncbc@rncbc.org>
2020-05-04 15:25:38 +0100

    - Fixed initial DCF1, LFO1 group enablement (GUI).

--- src/synthv1widget_param.cpp
+++ src/synthv1widget_param.cpp
@@ -961,6 +961,7 @@ synthv1widget_group::synthv1widget_group ( QWidget *pParent )
 
 	m_pParam = new synthv1widget_param(this);
 	m_pParam->setToolTip(QGroupBox::toolTip());
+	m_pParam->setValue(0.5f); // HACK: half-way on.
 
 	QObject::connect(m_pParam,
 		 SIGNAL(valueChanged(float)),
