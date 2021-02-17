3df0f03fda89ddf5eede93ad3fb493b6a863d0b0
rncbc <rncbc@rncbc.org>
2021-02-17 12:08:29 +0000

    - Make silent warning on unused return value.

--- src/synthv1_jack.cpp
+++ src/synthv1_jack.cpp
@@ -708,7 +708,7 @@ static void synthv1_sigterm_handler ( int /*signo*/ )
 {
 	char c = 1;
 
-	(::write(g_fdSigterm[0], &c, sizeof(c)) > 0);
+	(void) (::write(g_fdSigterm[0], &c, sizeof(c)) > 0);
 }
 
 #endif	// HAVE_SIGNAL_H
