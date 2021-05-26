62d8532dc6940c98d8752a51db96ff8bc2690112
rncbc <rncbc@rncbc.org>
2021-05-26 22:54:37 +0100

    - Force QT_QPA_PLATFORM=xcb to standalone GUI aplications.

--- src/synthv1_jack.cpp
+++ src/synthv1_jack.cpp
@@ -733,6 +733,7 @@ synthv1_jack_application::synthv1_jack_application ( int& argc, char **argv )
 	}
 
 	if (m_bGui) {
+		::setenv("QT_QPA_PLATFORM", "xcb", 0);
 	#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
 	#if QT_VERSION <  QT_VERSION_CHECK(6, 0, 0)
 		QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
