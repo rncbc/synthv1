b48236860e52a10e2a40b8560863ae38132d3ad6
rncbc <rncbc@rncbc.org>
2024-01-03 14:48:27 +0000

    - Just added a new-line to about-box text for padding.

--- src/synthv1widget.cpp
+++ src/synthv1widget.cpp
@@ -1554,6 +1554,7 @@ void synthv1widget::helpAbout (void)
 	sText += tr("This program is free software; you can redistribute it and/or modify it") + "<br />\n";
 	sText += tr("under the terms of the GNU General Public License version 2 or later.");
 	sText += "</small>";
+	sText += "<br />\n";
 	sText += "</p>\n";
 
 	QMessageBox::about(this, tr("About"), sText);
