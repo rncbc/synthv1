d43d868b559f5d00aa082f886ea58d0b4c36011c
rncbc <rncbc@rncbc.org>
2026-02-11 16:33:56 +0000

    - DejaVuSans TTF font is now firmly embedded to improve on the   self-containment status (ie. freetype2 static linking).

--- src/synthv1widget.cpp
+++ src/synthv1widget.cpp
@@ -1,7 +1,7 @@
 // synthv1widget.cpp
 //
 /****************************************************************************
-   Copyright (C) 2012-2025, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2012-2026, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -42,6 +42,8 @@
 #include <QShowEvent>
 #include <QHideEvent>
 
+#include <QFontDatabase>
+
 #include <cmath>
 
 #include <random>
@@ -56,6 +58,8 @@ synthv1widget::synthv1widget ( QWidget *pParent )
 	: QWidget(pParent), p_ui(new Ui::synthv1widget), m_ui(*p_ui)
 {
 	Q_INIT_RESOURCE(synthv1);
+
+	QFontDatabase::addApplicationFont(":/fonts/DejaVuSans.ttf");
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
 	// HACK: Dark themes grayed/disabled color group fix...
